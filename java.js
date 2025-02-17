e4190218301e28831db7a3e9f80bd5384cdd5b2a
Noirnoir24 <184695851+Noirnoir24@users.noreply.github.com>
2025-02-17 11:13:48 -0600

    add humanChoice function initialize score variables

--- java.js
+++ java.js
@@ -2,17 +2,39 @@ let max= 3;
 let min= 1;
 
 function getComputerChoice() {
-    let rando= Math. floor(Math. random() * (max - min + 1)) + min;
-    switch (rando) {
+    let computerChoice= Math. floor(Math. random() * (max - min + 1)) + min;
+    switch (computerChoice) {
         case 1: 
-        rando="Rock"
+        computerChoice="Rock"
         break;
         case 2: 
-        rando="Paper"
+        computerChoice="Paper"
         break;
         case 3:
-        rando="Scissors"
+        computerChoice="Scissors"
         break;
     }
-    return rando
+    return computerChoice
 }
+
+function caseIns(choice) {
+    return choice.toLowerCase();
+}
+
+function getHumanChoice() {
+    let humanChoice= prompt("Rock, paper, Scissors?");
+    humanChoice= caseIns(humanChoice);
+    if (humanChoice !== "rock" && humanChoice !== "scissors" && humanChoice !== "paper") {
+        humanChoice= "That is not an option!"
+        return humanChoice
+    }
+    else { 
+        return  humanChoice
+    }}
+
+let humanScore=0
+let computerScore=0
+
+function playRound(humanChoice,computerChoice) {
+    if (humanChoice=== rock && computerChoice===scissors)
+ return 0 }
