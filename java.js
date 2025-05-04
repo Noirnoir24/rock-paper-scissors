42ecda5f75df97449a8adff8e120ede551437f43
Noirnoir24 <184695851+Noirnoir24@users.noreply.github.com>
2025-05-04 09:10:09 -0600

    Reworks entire logic for the game
    The user input is now received through events
    relies on conditionals to determine winner
    no longer relies on loops

--- java.js
+++ java.js
@@ -1,3 +1,4 @@
+
 let max= 3;
 let min= 1;
 
@@ -5,87 +6,96 @@ function getComputerChoice() {
     let computerChoice= Math. floor(Math. random() * (max - min + 1)) + min;
     switch (computerChoice) {
         case 1: 
-        computerChoice="rock"
+        computerChoice="Rock"
         break;
         case 2: 
-        computerChoice="paper"
+        computerChoice="Paper"
         break;
         case 3:
-        computerChoice="scissors"
+        computerChoice="Scissors"
         break;
     }
     return computerChoice
 }
 
-function caseIns(choice) {
-    return choice.toLowerCase();
-}
+let humanChoice
 
-function getHumanChoice() {
-    let humanChoice= prompt("Rock, paper, Scissors?");
-    humanChoice= caseIns(humanChoice);
-    if (humanChoice !== "rock" && humanChoice !== "scissors" && humanChoice !== "paper") {
-        humanChoice= "That is not an option!"
-        return humanChoice
-    }
-    else { 
-        return  humanChoice
-    }}
+let humanScore=0
+let computerScore=0
 
-const humanSelection= getHumanChoice();
-const computerSelection= getComputerChoice();
 
-function playGame() {
-    let humanScore=0
-    let computerScore=0
-
-    function playRound(humanChoice,computerChoice) {
-        let draw= "Draw!"
-        let winner= "You win!"
-        let loser= "You lose..."
-        if (humanChoice=== "rock" && computerChoice=== "scissors") {
-            humanScore= humanScore+1
-            console.log (winner)
-            console.log("Your score: "+humanScore, "Computer Score: "+computerScore)
-            
-        }
-        else if (humanChoice=== "scissors" && computerChoice==="paper") {
-            humanScore= humanScore+1
-            console.log (winner)
-            console.log("Your score: "+humanScore, "Computer Score: "+computerScore)
-        }
-        else if (humanChoice=== "paper" && computerChoice==="rock") {
-            humanScore= humanScore+1
-            console.log (winner)
-            console.log("Your score: "+humanScore, "Computer Score: "+computerScore)
-        }
-        else if (humanChoice === computerChoice) {
-            console.log (draw)
-            console.log("Your score: "+humanScore, "Computer Score: "+computerScore)
-        }
-        else {
-            computerScore= computerScore+1
-            console.log (loser)
-            console.log("Your score: "+humanScore, "Computer Score: "+computerScore)
-            }
-       }
-
-    playRound(humanSelection,computerSelection);
-    playRound(getHumanChoice(),getComputerChoice())
-    playRound(getHumanChoice(),getComputerChoice())
-    playRound(getHumanChoice(),getComputerChoice())
-    playRound(getHumanChoice(),getComputerChoice())
-
-    if(humanScore>computerScore) {
+    if(humanScore>4) {
         console.log("Player wins!")
     }
-    else if (computerScore>humanScore) {
+    else if (computerScore>4) {
         console.log("Computer Wins!")
     }
-    else {
-        console.log("Draw!")
-    }
-}
+    
 
-playGame();
+
+
+const buttons= document.querySelectorAll("button");
+for (const button of buttons) {
+    button.addEventListener("click", () => {
+        humanChoice= button.textContent;
+        playRound(humanChoice, getComputerChoice())
+        const result= document.querySelector("#announcer")
+        if(humanScore>4) {
+            result.textContent="Player wins!"
+        }
+        else if (computerScore>4) {
+            result.textContent="Computer Wins!"
+        }
+
+        if (humanScore > 4 || computerScore > 4) {
+            buttons.forEach(btn => btn.disabled = true);
+        }
+})
+}
+    
+
+function playRound(humanChoice,computerChoice) {
+    let draw= "Draw!"
+    let winner= "You win!"
+    let loser= "You lose..."
+
+    const result= document.querySelector("#announcer")
+
+    
+    if (humanChoice=== "Rock" && computerChoice=== "Scissors") {
+        humanScore= humanScore+1
+        result.textContent= (winner + " Your score: "+humanScore + " Computer Score: "+computerScore)
+        console.log (winner)
+        console.log("Your score: "+humanScore, "Computer Score: "+computerScore)
+        
+    }
+    else if (humanChoice=== "Scissors" && computerChoice==="Paper") {
+        humanScore= humanScore+1
+        result.textContent= (winner + " Your score: "+humanScore + " Computer Score: "+computerScore)
+        console.log (winner)
+        console.log("Your score: "+humanScore, "Computer Score: "+computerScore)
+    }
+    else if (humanChoice=== "Paper" && computerChoice==="Rock") {
+        humanScore= humanScore+1
+        result.textContent= (winner + " Your score: "+humanScore + " Computer Score: "+computerScore)
+        console.log (winner)
+        console.log("Your score: "+humanScore, "Computer Score: "+computerScore)
+    }
+    else if (humanChoice === computerChoice) {
+        result.textContent= (draw + " Your score: "+humanScore + " Computer Score: "+computerScore)
+        console.log (draw)
+        console.log("Your score: "+humanScore, "Computer Score: "+computerScore)
+    }
+    else {
+        computerScore= computerScore+1
+        result.textContent= (loser + " Your score: "+humanScore + " Computer Score: "+computerScore)
+        console.log (loser)
+        console.log("Your score: "+humanScore, "Computer Score: "+computerScore)
+        }
+   }
+
+        
+        
+
+
 
